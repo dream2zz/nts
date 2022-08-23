a7463c8a61b6e9ed8fb8dc9b4e2f902f795d9380
dream <zhu1531@163.com>
2022-08-23 13:17:32 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { createSSRApp } from "vue";
 import App from "./App.vue";
-
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles/main.css'
 import { createVuetify } from 'vuetify'
-import 'vuetify/styles/main.css' // Global CSS has to be imported
-const vuetify = createVuetify() 
+
+const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+  },
+}) 
 
 export function createApp() {
   const app = createSSRApp(App);
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,30 +1,28 @@
 <template>
+  <v-toolbar>
+    <span class="ma-3">杭州</span>
+    <v-text-field dense hide-details append-icon="mdi-magnify" ></v-text-field>
+    <v-btn icon>
+      <v-icon icon="mdi-line-scan" />
+    </v-btn>
+  </v-toolbar>
 
-    <v-carousel
-      cycle
-      hide-delimiter-background
-      height="100"
-      class="pa-3"
-      show-arrows="hover"
-    >
-      <v-carousel-item v-for="(slide, i) in slides" :key="i">
-        <v-sheet :color="colors[i]" height="100%">
-          <div class="d-flex fill-height justify-center align-center">
-            <div class="text-h2">{{ slide }} Slide</div>
-          </div>
-        </v-sheet>
-      </v-carousel-item>
-    </v-carousel>
+  <v-carousel cycle height="190" hide-delimiter-background show-arrows-on-hover delimiter-icon="mdi-minus" :show-arrows="false">
+    <v-carousel-item v-for="(item, i) in images" :key="i" :src="item.src"></v-carousel-item>
+  </v-carousel>
 
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 
-const title = ref("首页");
 const images = [
-  "https://fastly.jsdelivr.net/npm/@vant/assets/apple-1.jpeg",
-  "https://fastly.jsdelivr.net/npm/@vant/assets/apple-2.jpeg",
+  {
+    src: 'https://tdesign.gtimg.com/site/swiper/02.png',
+  },
+  {
+    src: 'https://tdesign.gtimg.com/site/swiper/03.png',
+  },
 ];
 
 const colors = [
@@ -38,5 +36,4 @@ const slides = ["First", "Second", "Third", "Fourth", "Fifth"];
 </script>
 
 <style>
-
 </style>
